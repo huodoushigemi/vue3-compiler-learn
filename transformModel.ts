6c22763e7cd733ff09546340769f5739e5799f93
huodoushigemi <2629878269@qq.com>
2023-04-24 13:27:08 +0800

    1 file modified

--- transformModel.ts
+++ transformModel.ts
@@ -6,7 +6,11 @@ export const transformModel: NodeTransform = node => {
   if (node.type != ELEMENT) return
 
   const VModel = findDir(node, 'model')
-  if (!VModel || VModel.arg != null) return
 
-  VModel.arg = createSimpleExpression('value', true, undefined, 3)
+  // v-model 没有传入绑定的属性，则将属性绑定到 value
+  // e.g. v-model => v-model:value
+  // e.g. v-model:xxx => v-model:xxx
+  if (VModel && VModel.arg == null) {
+    VModel.arg = createSimpleExpression('value', true, undefined, 3)
+  }
 }
